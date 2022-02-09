80f0d3feae35b88f15a9c32a0d72a5f8b26ae724
saltysam14 <92763644+saltysam14@users.noreply.github.com>
2022-02-08 21:15:39 -0500

    edited App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,40 @@ import { useEffect, useMemo, useState } from "react";
 import { ThirdwebSDK } from "@3rdweb/sdk";
 import { useWeb3 } from "@3rdweb/hooks";
 
+const sdk = new ThirdwebSDK("rinkeby");
+
+const bundleDropModule = sdk.getBundleDropModule(
+  "0x1060B79350E8Cfe42afb3e54D9cBad24eae42c50"
+);
+
 const App = () => {
   const { connectWallet, address, error, provider } = useWeb3();
   console.log("👋 Address:", address);
+
+  const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+
+  useEffect(() => {
+    if (!address) {
+      return;
+    }
+  }
+  
+  return bundleDropModule
+    .balanceOf(addresss, "0")
+    .then((balance) => {
+      if (balance.gt(0)) {
+        setHasClaimedNFT(true);
+        console.log("🌟 this user has a membership NFT!")
+      } else {
+        setHasClaimedNFT(false);
+        console.log("😭 this user doesn't have a membership NFT.")
+      }
+    })
+    .catch((error) => {
+      setHasClaimedNFT(false);
+      console.error("failed to nft balance", error);
+    });
+  }, [address]);
 
   if (!address) {
     return (
